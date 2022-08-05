17b449b611568436f4fb425231afb275fac972a0
Mgt789 <dmeher082@gmail.com>
2022-08-05 12:15:09 +0530

    part3-5

---
+++ third part/jQuery MouseMove Events Listeners and hover Events/app23.js
@@ -0,0 +1,44 @@
+$('.main div').addClass('box').hover(function () {
+    $(this).css('background-color', 'red')
+},
+function () {
+    $(this).css('background-color', 'white')
+}).mouseenter(function(e){
+   // $(this).text(`(X:${e.pageX} Y:${e.pageY})`);
+})
+
+$('button').css('width','100px');
+
+$('button').mouseenter(function (e) {
+    $(this).css('background-color', 'red')
+})
+
+$('button').mouseleave(function () {
+    $(this).css('background-color', 'white')
+})
+
+$(document).mousemove(function(e){
+    $('.div1').text(`(X:${e.pageX} Y:${e.pageY})`);
+})
+
+//display text
+$('.main').mouseout(()=>{
+    $('.div2').text('Mouse OUT on MAIN!')
+})
+$('.main').mouseover(()=>{
+    $('.div2').text('Mouse OVER on MAIN!')
+})
+$('.main').mouseleave(()=>{
+    $('.div3').text('Mouse LEAVE on MAIN!')
+})
+$('.main').mouseenter(()=>{
+    $('.div3').text('Mouse ENTER on MAIN!')
+})
+//mouse up down function
+$('.main div').mouseup(function(){
+    $(this).text('MOUSE UP');
+    $(this).css('background-color', 'blue');
+}).mousedown(function(){
+    $(this).text('MOUSE DOWN');
+    $(this).css('background-color', 'green');
+})
